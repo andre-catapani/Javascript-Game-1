1da9de8f9d39dfe1aa8f9d0f2facf00223f17787
andre-catapani <andrefrc@outlook.com>
2023-02-23 20:06:37 -0300

    create the animation loop using sprite

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ const CANVAS_HIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0; 
+let gameFrame = 0;
+const staggerFrames = 4;
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HIGHT);
-    ctx.fillRect(50, 50, 100, 100);
+    // ctx.fillRect(50, 50, 100, 100);
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0){
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
